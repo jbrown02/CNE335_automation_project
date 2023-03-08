6839265e10f2eef3c9089949314fb879a8ee12a5
jbrown02 <116325511+jbrown02@users.noreply.github.com>
2023-03-07 18:51:52 -0800

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,10 +1,20 @@
+# Joshua Brown
+# 3/7/23 - Winter
+# CNE 335 - Server Automation
+
+import os
+
 class Server:
     """ Server class for representing and manipulating servers. """
 
     def __init__(self, server_ip):
-        # TODO -
         self.server_ip = server_ip
 
     def ping(self):
-        # TODO - Use os module to ping the server
-        return
+        response = os.system("ping -c 1 " + self)
+        if response == 0:
+            return True
+        else:
+            return False
+
+print(myping("54.245.39.214"))
