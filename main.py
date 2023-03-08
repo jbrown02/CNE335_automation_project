af40b02d8414e781ea350cfc0705ba8cb9ff4ad3
jbrown02 <116325511+jbrown02@users.noreply.github.com>
2023-03-07 19:54:39 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@
 # 3/7/23
 # CNE 335 Winter - Final Project
 
+from Server import Server
+
 def print_program_info():
     print("Server Automator v0.1 by Josh Brown")
 
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
-
-from Server import Server
 
 print(Server.ping("54.245.39.214"))
