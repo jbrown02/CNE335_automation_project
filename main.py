7228bd3807eff2f49e0a792eb123a4215c18fca6
jbrown02 <116325511+jbrown02@users.noreply.github.com>
2023-03-07 18:36:55 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-# This is the template code for the CNE335 Final Project
-# Justin Ellis
-# CNE 335 Fall
+# Joshua Brown
+# 3/7/23
+# CNE 335 Winter - Final Project
 
 def print_program_info():
-    # TODO - Change your name
-    print("Server Automator v0.1 by Your Name")
+    print("Server Automator v0.1 by Josh Brown")
 
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
-    # TODO - Create a Server object
-    # TODO - Call Ping method and print the results
+
+# Server object created in ECS
+
+import os
+
+def myping(host):
+    response = os.system("ping -c 1 " + host)
+    
+    if response == 0:
+        return True
+    else:
+        return False
+        
+print(myping("54.245.39.214"))
